c05d132d2195c3ff275b66cc749ffd49267ed903
lenardry <60217876+lenardry@users.noreply.github.com>
2020-09-22 13:28:08 +0800

    Add files via upload

---
+++ deframing.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+	char input[2000]="", dataAll[2000]="", startbit='0', stopbit='1';
+	char delim[]="||";
+	int len;
+	
+	//printf("Input frame: ");
+	scanf("%s", &input);
+	
+	char *frame = strtok(input, delim);
+	
+	printf("\n");
+	while(frame!=NULL){
+		int len = strlen(frame), i=0;
+		char data[300] = "";
+		while(i!=len){
+			if((i==len-1) && (frame[i]==stopbit)){ //validates if loop reached the stop bit
+				break;
+			}
+			else if ((frame[0]=='0')&&(i!=0)){ //validates the start bit and that the index entails a data bit
+				data[i-1] = frame[i]; 
+			}
+			i++;
+		}
+		strcat(data, delim); //concatenates "||" to end of data
+		strcat(dataAll, data); //concatenates this data to whole data set
+		frame = strtok(NULL, delim);
+	}
+	
+	
+	//printf("Data and checksum: ");
+	printf("\n");
+	printf("%s\n", dataAll); //display data appended with checksum
+	
+	
+}
